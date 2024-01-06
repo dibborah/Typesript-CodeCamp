c7ba1c721fa19b17b173902edd3215c6c2541de0
dibborah <dibyajyotiborah469@gmail.com>
2024-01-06 18:33:59 +0530

    first commit

---
+++ 01INTRO/myvariables.ts
@@ -0,0 +1,7 @@
+// let user:string;
+// user = "dibya"
+let user = "dibya"// This code depicts that typescript is smart
+// user = 5;Not allowed
+// user = "dib"// allowed
+
+console.log(user);
---
+++ 02BASICS/myfunctions.ts
@@ -0,0 +1,25 @@
+// let's see a different senerio
+
+// Here the arguement type is discussed along with the return type
+
+// function addOne(num: number):number{
+//     return num + 1;
+// }
+
+// addOne(1);
+
+// This is a different senerio where union should be used 
+// We'll discuss it later
+
+// function getValue(myVal:number){
+//     if(myVal>5){
+//         return true;
+//     }
+//     return "200 OK"
+// }
+
+// Giving the parameter type & return type in an ARROW FUNCTION
+
+const getHello = (val: string):string => {
+    return ""
+}
