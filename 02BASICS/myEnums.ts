20fce1d5e70243cf1f638e4028754a7aeb94cc73
dibborah <dibyajyotiborah469@gmail.com>
2024-01-10 21:47:31 +0530

    myEnums almost done

--- 02BASICS/myEnums.ts
+++ 02BASICS/myEnums.ts
@@ -1,2 +1,10 @@
 // Enums: Enumerations => Meaning: Specifically Listed
 
+const enum seatChoice {
+    AISLE = "AISLE",
+    MIDDLE = 2,
+    WINDOW = "",
+    FOURTH = "FOUR"
+}
+
+const seat = seatChoice.AISLE;
