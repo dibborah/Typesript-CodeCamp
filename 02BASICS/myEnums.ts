ed9fceeb130d9a823bf82ce64a98f051d651b2c4
dibborah <dibyajyotiborah469@gmail.com>
2024-01-10 21:53:40 +0530

    added some notes comments of Enums

--- 02BASICS/myEnums.ts
+++ 02BASICS/myEnums.ts
@@ -1,5 +1,12 @@
 // Enums: Enumerations => Meaning: Specifically Listed
 
+// Enums are used to give restrictions in choice
+// When Enum used one has to choose between only the selected types
+// It is like object just the equal(=) sign is not used => That's a synta difference with objects ({})
+// When assigned STRING value is assigned the value below it has to be assigned with some value
+// WhereAs if a NUMBER value is assigned than a value assigned criteria is not mandatory in Enum(since it automatically assigns than)
+// When number are assigned it assignes whatever value are below it by itself based on an ascending order
+
 const enum seatChoice {
     AISLE = "AISLE",
     MIDDLE = 2,
