b79f47c3e24ffcb5da3844bc6ce0de1c396ecbf4
dibborah <dibyajyotiborah469@gmail.com>
2024-01-12 14:19:47 +0530

    Started to learn How to setup Typescipt for Real Projects

---
+++ 03Purets/Explanations.ts
@@ -0,0 +1,22 @@
+// Installing important file for setting up typescript for any project(PRODUCTION LEVEL)
+// #Note: Use GIT BASH terminal to execute this commands
+
+// 1.
+// Command for installing tsconfig.json : tsc --init
+// tsConfig.json : It is for tsConfig
+// Typescipt configuration File
+
+// 2.
+// command for installing package.json : npm init -y
+// Generate node package.json file
+// For installing any NODE package manager or npm
+// npm (Node Package Manager)
+
+// In case of PRODUCTION we have two folder
+// 1. src (source): src is where is write our code
+// 2. dist (distribution): This is something which gets on to the production and that is being served to the end user
+
+// command for installing this two folders: mkdir src dist // See how this space is used in the command to install more than one folder or package in a single command itself
+
+
+
