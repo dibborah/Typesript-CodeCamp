56f300651d67301b10566ea70121df3dda94930e
dibborah <dibyajyotiborah469@gmail.com>
2024-01-18 21:41:36 +0530

    private and public data modifiers

--- 03Purets/dist/index.js
+++ 03Purets/dist/index.js
@@ -1,10 +1,24 @@
 "use strict";
+// class User {
+//     public name: string
+//     email:string // The ones which are not marked by any data modifier are automatically intrinsically marked as public by Typescript
+//     private readonly city:string = "Jaipur";
+//     constructor(email:string, name:string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+// const dibya = new User("dib@d.co", "dibya");
 class User {
-    constructor(email, name) {
-        this.city = "Jaipur";
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = "Jaipur";
     }
 }
-const dibya = new User("dib@d.co", "dibya");
-// dibya.city = "Delhi"
+const dibya = new User("dib@d.co", "dibya", "123");
+// dibya.city; // Not accessible out the constructor function since the city property is made private
+// Note: 
+// 1. private is a Typescript world 
+// 2. # hash is a Javascript world
--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -1,12 +1,28 @@
+// class User {
+//     public name: string
+//     email:string // The ones which are not marked by any data modifier are automatically intrinsically marked as public by Typescript
+//     private readonly city:string = "Jaipur";
+//     constructor(email:string, name:string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+
+// const dibya = new User("dib@d.co", "dibya");
 class User {
-    name:string
-    email:string
-    readonly city:string = "Jaipur";
-    constructor(email:string, name:string){
-        this.email = email
-        this.name = name
+    private readonly city:string = "Jaipur";
+    constructor(
+        public email:string, 
+        public name:string,
+        private userId:string,
+        ){// short syntax where this is no need to be written
     }
 }
 
-const dibya = new User("dib@d.co", "dibya");
-// dibya.city = "Delhi"
+const dibya = new User("dib@d.co", "dibya", "123");
+
+// dibya.city; // Not accessible out the constructor function since the city property is made private
+
+// Note: 
+// 1. private is a Typescript world 
+// 2. # hash is a Javascript world
