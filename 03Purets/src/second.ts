52f773c9dedbb7582ed75bef1f8b20a9b392dbb5
dibborah <dibyajyotiborah469@gmail.com>
2024-01-31 01:06:15 +0530

    Completed why interface is important

--- 03Purets/src/second.ts
+++ 03Purets/src/second.ts
@@ -13,11 +13,23 @@ class Instagram implements TakePhoto {// What you do w/ TakePhoto is not cared b
     ){}
 }
 
-class Youtube implements TakePhoto{
+interface Story {
+    createStory(): void
+    // createStory: () => void
+}
+
+class Youtube implements TakePhoto, Story{ // Since class is a template so we need to and we are going to create instances by it so being a template like types also has to be defined even if it is just implementing an interface with the already pre-defined types
     constructor(
         public cameraMode:string,
         public filter:string,
         public burst:number,
+        public short: string,
     ){}
+
+    createStory(): void {
+        console.log("Story was created");
+    }
 }
+
+
 
