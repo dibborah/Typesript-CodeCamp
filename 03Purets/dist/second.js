57439f7c644cb446cbe10f3976f48fecb43c24a8
dibborah <dibyajyotiborah469@gmail.com>
2024-01-31 01:13:06 +0530

    just added the second.js file in dist folder

--- 03Purets/dist/second.js
+++ 03Purets/dist/second.js
@@ -3,7 +3,6 @@
 //     filter: string,
 //     burst: number,
 // }
-
 // class Instagram implements TakePhoto {// What you do w/ TakePhoto is not cared by interface and that is your own business
 //     // Interface is when declaring one has to follow this rules
 //     constructor(
@@ -12,12 +11,10 @@
 //         public burst:number,
 //     ){}
 // }
-
 // interface Story {
 //     createStory(): void
 //     // createStory: () => void
 // }
-
 // class Youtube implements TakePhoto, Story{ // Since class is a template so we need to and we are going to create instances by it so being a template like types also has to be defined even if it is just implementing an interface with the already pre-defined types
 //     constructor(
 //         public cameraMode:string,
@@ -25,11 +22,7 @@
 //         public burst:number,
 //         public short: string,
 //     ){}
-
 //     createStory(): void {
 //         console.log("Story was created");
 //     }
 // }
-
-
-
