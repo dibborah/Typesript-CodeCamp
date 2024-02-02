28a9599cf7a8853c798c191e77f3c65d4e06bde3
dibborah <dibyajyotiborah469@gmail.com>
2024-02-02 19:05:49 +0530

    private, getters and setters and Protected Access modifier of classes

--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -11,19 +11,50 @@
 // const dibya = new User("dib@d.co", "dibya");
 class User {
     private readonly city:string = "Jaipur";
+    private _courseCount = 1;
+    protected _courseNumber = 2;
     constructor(
         public email:string, 
         public name:string,
         private userId:string,
         ){// short syntax where this is no need to be written
     }
+
+    private deleteToken(){
+        console.log("This is just a delete token private method");        
+    }
+    
     // getAppleEmail: () => string,
-    getAppleEmail():string{
-        return "";
+    get getAppleEmail(): string{// getAppleEmail is a get accessor
+        return `apple${this.email}`;
+    }
+    get getCourseCount(): number{
+        return this._courseCount;
+    }
+    set courseCount(courseNum:number){
+        if(courseNum <= 1){
+            throw new Error("CourseNum must be greater than 1");
+        }
+        this._courseCount = courseNum;
+    }    
+}
+
+class SubUser extends User {
+    changeCourse(){
+        // this._courseCount = 1;// This throws error as _courseCount is a private property and can only be accessible within class User and not also on any subclass created from User like SubUser
+        this._courseNumber = 1;
     }
 }
 
 const dibya = new User("dib@d.co", "dibya", "123");
+const dib = new SubUser("dib@d.co", "dibya", "123");
+console.log(dibya.getAppleEmail);
+// console.log(dibya._courseCount);
+// console.log(dib._courseNumber);// This throws error as _courseNumber is a protected class and can only be accessed within class User and the subclasses of User
+
+
+// dibya.deleteToken();// This throws error as deleteToken is a private method and only accessible within class
+// dibya._courseCount;// This is also a private property so can't be accessible outside class
 
 // dibya.city; // Not accessible out the constructor function since the city property is made private
 
